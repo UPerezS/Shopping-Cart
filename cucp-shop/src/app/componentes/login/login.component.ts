f8207fa01b81405308e4759cc9d5c10ebe2423c7
UPerezS <cama8836@gmail.com>
2024-06-06 15:23:38 -0600

    Formulario del Login con Validaciones Completado

--- cucp-shop/src/app/componentes/login/login.component.ts
+++ cucp-shop/src/app/componentes/login/login.component.ts
@@ -1,20 +1,70 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   
-  loginForm = this.fb.group({
-    email: ['',[Validators.required, Validators.email]],
-    password: ['', Validators.required]
-  })
+  loginForm: FormGroup;
 
-  constructor(private fb:FormBuilder){
-    
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      nombreCompleto: ['', Validators.required],
+      usuario: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [
+        Validators.required,
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*\\d).{8,}$')
+      ]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: this.mustMatch('password', 'confirmPassword')
+    });
   }
 
+  get nombreCompleto(): AbstractControl {
+    return this.loginForm.get('nombreCompleto')!;
+  }
+
+  get usuario(): AbstractControl {
+    return this.loginForm.get('usuario')!;
+  }
+
+  get email(): AbstractControl {
+    return this.loginForm.get('email')!;
+  }
+
+  get password(): AbstractControl {
+    return this.loginForm.get('password')!;
+  }
+
+  get confirmPassword(): AbstractControl {
+    return this.loginForm.get('confirmPassword')!;
+  }
+
+  mustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors['mustMatch']) {
+        return;
+      }
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form Submitted', this.loginForm.value);
+    }
+  }
 }
