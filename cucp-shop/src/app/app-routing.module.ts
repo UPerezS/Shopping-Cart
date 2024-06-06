8df46ab2a56b691d61084c75b661c1bd8f37ce50
UPerezS <cama8836@gmail.com>
2024-06-06 17:40:25 -0600

    Se Agrego el Home y Cierre de Sesión

--- cucp-shop/src/app/app-routing.module.ts
+++ cucp-shop/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login.component';
 import { RegisterComponent } from './componentes/register/register.component';
 import { ProductoListaComponent } from './componentes/producto-lista/producto-lista.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 const routes: Routes = [
   { path:'login', component: LoginComponent },
   { path:'register', component: RegisterComponent },
-  { path:'home', component: ProductoListaComponent }
+  { path:'home', component: HomeComponent },
+  { path:'', component: ProductoListaComponent }
 ];
 
 @NgModule({
